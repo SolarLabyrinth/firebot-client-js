43746bde4157c30802b177937539802f64b561d5
SolarLabyrinth <thesolarlabyrinth@gmail.com>
2025-09-01 13:48:00 -0500

    first few endpoints

---
+++ src/client.ts
@@ -0,0 +1,134 @@
+import { z } from "zod/mini";
+
+const GetStatusSchema = z.object({
+  connections: z.object({
+    chat: z.boolean(),
+  }),
+});
+
+const EffectSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  description: z.string(),
+  icon: z.string(),
+  categories: z.optional(z.array(z.string())),
+  dependencies: z.optional(
+    z.union([
+      z.array(z.string()),
+      z.object({
+        twitch: z.boolean(),
+      }),
+    ])
+  ),
+  outputs: z.optional(
+    z.array(
+      z.object({
+        label: z.string(),
+        description: z.string(),
+        defaultName: z.string(),
+      })
+    )
+  ),
+  exemptFromTimeouts: z.optional(z.boolean()),
+  triggers: z.optional(
+    z.object({
+      command: z.boolean(),
+      event: z.array(z.string()),
+    })
+  ),
+  hidden: z.optional(z.boolean()),
+});
+
+const GetEffectsSchema = z.array(EffectSchema);
+const GetEffectSchema = EffectSchema;
+
+const GetPresetEffectListsSchema = z.array(
+  z.object({
+    id: z.string(),
+    name: z.string(),
+    args: z.array(z.string()),
+  })
+);
+
+class FireBotApi {
+  private baseUrl: string;
+
+  constructor(baseUrl: string) {
+    this.baseUrl = baseUrl;
+  }
+
+  private async get(endpoint: string) {
+    const url = new URL(endpoint, this.baseUrl);
+    const response = await fetch(url.toString(), {
+      method: "GET",
+    });
+    const json = await response.json();
+    return json;
+  }
+  private async post(endpoint: string, body: unknown) {
+    const url = new URL(endpoint, this.baseUrl);
+    const response = await fetch(url.toString(), {
+      method: "POST",
+      body: JSON.stringify(body),
+    });
+    const json = await response.json();
+    return json;
+  }
+
+  // Status
+
+  async getStatus() {
+    const json = await this.get("/api/v1/status");
+    const data = GetStatusSchema.parse(json);
+    return data;
+  }
+
+  // Effects
+
+  async getEffects() {
+    const json = await this.get("/api/v1/effects");
+    const data = GetEffectsSchema.parse(json);
+    return data;
+  }
+  async getEffect(id: string) {
+    const json = await this.get(`/api/v1/effects/${id}`);
+    const data = GetEffectSchema.parse(json);
+    return data;
+  }
+
+  async getPresetEffectLists() {
+    const json = await this.get(`/api/v1/effects/preset`);
+    const data = GetPresetEffectListsSchema.parse(json);
+    return data;
+  }
+
+  // Custom Variables
+
+  async getCustomVariables() {
+    const json = await this.get(`/api/v1/custom-variables`);
+    // const data = GetPresetEffectListsSchema.parse(json);
+    return json;
+  }
+  async getCustomVariable(name: string) {
+    const json = await this.get(`/api/v1/custom-variables/${name}`);
+    // const data = GetPresetEffectListsSchema.parse(json);
+    return json;
+  }
+  async setCustomVariable(name: string, value: any, ttl: number) {
+    const json = await this.post(`/api/v1/custom-variables/${name}`, {
+      data: value,
+      ttl,
+    });
+    // const data = GetPresetEffectListsSchema.parse(json);
+    return json;
+  }
+}
+
+// const firebot = new FireBotApi("http://localhost:7472");
+// console.log(await firebot.getEffects());
+// console.log(await firebot.getEffect("firebot:obs-set-browser-source-url"));
+// console.log(await firebot.getPresetEffectLists());
+// console.log(await firebot.getCustomVariable("test"));
+// console.log(await firebot.setCustomVariable("test", "example", 1000));
+// console.log(await firebot.getCustomVariable("test"));
+// console.log(await firebot.getCustomVariables());
